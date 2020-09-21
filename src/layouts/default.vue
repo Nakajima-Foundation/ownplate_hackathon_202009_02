c2e98f018a27cc57c5fd13909aa55bb7ff14dad0
Yuki Takahashi <yuki0627@gmail.com>
2020-09-22 07:04:16 +0900

    change support message demo

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -41,38 +41,45 @@
       <!-- Home -->
       <div class="align-center m-t-24">
         <router-link to="/">
-          <div class="op-button-medium tertiary w-192" @click="handleClose()">{{ $t("menu.home") }}</div>
+          <div class="op-button-medium tertiary w-192" @click="handleClose()">
+            {{ $t("menu.home") }}
+          </div>
         </router-link>
       </div>
 
       <!-- Profile -->
       <div class="align-center m-t-24">
         <router-link to="/u/profile">
-          <div class="op-button-small tertiary" @click="handleClose()">{{ $t("profile.title") }}</div>
+          <div class="op-button-small tertiary" @click="handleClose()">
+            {{ $t("profile.title") }}
+          </div>
         </router-link>
       </div>
 
       <!-- Terms for Restaurant -->
       <div class="align-center m-t-24">
         <router-link to="/terms/admin" target="_blank">
-          <div
-            class="op-button-text t-button"
-            @click="handleClose()"
-          >{{ $t("menu.termsRestaurant") }}</div>
+          <div class="op-button-text t-button" @click="handleClose()">
+            {{ $t("menu.termsRestaurant") }}
+          </div>
         </router-link>
       </div>
 
       <!-- Terms for User -->
       <div class="align-center">
         <router-link to="/terms/user" target="_blank">
-          <div class="op-button-text t-button" @click="handleClose()">{{ $t("menu.termsUser") }}</div>
+          <div class="op-button-text t-button" @click="handleClose()">
+            {{ $t("menu.termsUser") }}
+          </div>
         </router-link>
       </div>
 
       <!-- Privacy -->
       <div class="align-center">
         <router-link to="/privacy" target="_blank">
-          <div class="op-button-text t-button" @click="handleClose()">{{ $t("menu.privacy") }}</div>
+          <div class="op-button-text t-button" @click="handleClose()">
+            {{ $t("menu.privacy") }}
+          </div>
         </router-link>
       </div>
     </b-sidebar>
@@ -86,20 +93,29 @@
       :open.sync="this.supportOn"
     >
       <div class="m-l-8 m-r-8 align-center">
-        <div class="op-button-small tertiary" @click="supportClose()">サポートを閉じる</div>
+        <div class="op-button-small tertiary" @click="supportClose()">
+          サポートを閉じる
+        </div>
         <div
           class="bg-surface r-8 d-low m-t-8 p-b-24"
           style="height: 550px; border: 2px solid rgba(0, 0, 0, 0.2); overflow: scroll;"
         >
           <div
-            class="m-t-16"
+            class="box"
+            v-bind:class="supportMessage.role"
             v-for="supportMessage in this.supportMessages"
-            :key="supportMessage"
-          >{{ supportMessage }}</div>
+            :key="supportMessage.message"
+          >
+            {{ supportMessage.message }}
+          </div>
         </div>
         <div>画面: {{ this.supportFrom }}</div>
         <b-field label="メッセージ">
-          <b-input v-model="supportInput" maxlength="200" type="textarea"></b-input>
+          <b-input
+            v-model="supportInput"
+            maxlength="200"
+            type="textarea"
+          ></b-input>
         </b-field>
 
         <button @click="setSupportMessage">投稿</button>
@@ -109,7 +125,9 @@
     <!-- Main -->
     <div class="main">
       <div class="contents">
-        <div v-if="underConstruction" class="underConstruction">{{ $t("underConstruction") }}</div>
+        <div v-if="underConstruction" class="underConstruction">
+          {{ $t("underConstruction") }}
+        </div>
 
         <!-- approproate component under pages will be displayed -->
         <nuxt v-if="isReadyToRender"></nuxt>
@@ -118,7 +136,12 @@
     </div>
 
     <!-- Loading -->
-    <b-loading v-if="isLoading" :is-full-page="true" :active="true" :can-cancel="false"></b-loading>
+    <b-loading
+      v-if="isLoading"
+      :is-full-page="true"
+      :active="true"
+      :can-cancel="false"
+    ></b-loading>
 
     <!-- Footer -->
     <div class="m-t-48">
@@ -126,12 +149,19 @@
         <div class="flex-1">
           <div
             class="is-inline-block t-caption c-text-white-medium m-t-16 m-l-16"
-          >Operated by Singularity Society</div>
+          >
+            Operated by Singularity Society
+          </div>
         </div>
         <div class="align-right">
-          <div class="op-button-pill bg-sattle-white m-r-16 m-t-16" @click="openLang()">
+          <div
+            class="op-button-pill bg-sattle-white m-r-16 m-t-16"
+            @click="openLang()"
+          >
             <i class="material-icons c-text-white-high">language</i>
-            <span class="c-text-white-high t-button">{{ languages[language] }}</span>
+            <span class="c-text-white-high t-button">{{
+              languages[language]
+            }}</span>
             <i class="material-icons c-text-white-high">arrow_drop_down</i>
           </div>
         </div>
@@ -141,15 +171,30 @@
     <!-- Language Popup-->
     <b-modal :active.sync="langPopup" :width="488" scroll="keep">
       <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
-        <div class="t-h6 c-text-black-disabled p-b-8">{{ $t("menu.selectLanguage") }}</div>
-        <div class="m-t-16" v-for="(lang, lang_key) in languages" :key="lang_key">
-          <div class="op-button-pill bg-form" @click="changeLangAndClose(lang_key)">
-            <i class="material-icons c-text-black-high" v-if="lang_key == language">check</i>
+        <div class="t-h6 c-text-black-disabled p-b-8">
+          {{ $t("menu.selectLanguage") }}
+        </div>
+        <div
+          class="m-t-16"
+          v-for="(lang, lang_key) in languages"
+          :key="lang_key"
+        >
+          <div
+            class="op-button-pill bg-form"
+            @click="changeLangAndClose(lang_key)"
+          >
+            <i
+              class="material-icons c-text-black-high"
+              v-if="lang_key == language"
+              >check</i
+            >
             <span class="t-button">{{ lang }}</span>
           </div>
         </div>
         <div class="m-t-24 align-center">
-          <div class="op-button-small tertiary" @click="closeLang()">{{ $t("menu.close") }}</div>
+          <div class="op-button-small tertiary" @click="closeLang()">
+            {{ $t("menu.close") }}
+          </div>
         </div>
       </div>
     </b-modal>
@@ -171,6 +216,7 @@ export default {
   },
   data() {
     return {
+      isSupporter: true,
       language: "en",
       languages: [],
       items: [
@@ -494,5 +540,18 @@ export default {
   background: yellow;
   @extend .p-t-4;
   @extend .p-b-4;
+}
+
+.shop {
+  margin: 10px 0px 0px 15px;
+  background-color: rgba(0, 0, 0, 0.05);
+  text-align: left;
+  padding: 8px 2px 8px 8px;
+}
+.supporter {
+  margin: 15px 10px 0px 0px;
+  padding: 8px 8px 8px 2px;
+  background-color: darkseagreen;
+  text-align: left;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,171 +8,218 @@ Vue.use(Vuex);
 export const strict = false;
 
 export const state = () => ({
-	user: undefined, // undefined:not authorized, null:no user
-	claims: undefined, // custom claims
-	lang: undefined,
-	date: new Date(),
-	carts: {}, // for "Edit Order"
-	server: {}, // server configuration
-	orderEvent: 0,
-	orderObj: {},
-	soundEnable: false, // after user touch/click event, this flag set true (for mobile browser)
-	soundOn: false, // for restaurant admin config
-	soundFile: "",
-	isWindowActive: false, // active status of browser window
-	dialog: null, // for DialogBox
-	isLoading: false, // for full-page loading animation
-	isFirefoxPBM: undefined, // true, false, null
-	supportOn: false, // for support sidebar
-	supportFrom: "",
-	supportMessages: []
+  user: undefined, // undefined:not authorized, null:no user
+  claims: undefined, // custom claims
+  lang: undefined,
+  date: new Date(),
+  carts: {}, // for "Edit Order"
+  server: {}, // server configuration
+  orderEvent: 0,
+  orderObj: {},
+  soundEnable: false, // after user touch/click event, this flag set true (for mobile browser)
+  soundOn: false, // for restaurant admin config
+  soundFile: "",
+  isWindowActive: false, // active status of browser window
+  dialog: null, // for DialogBox
+  isLoading: false, // for full-page loading animation
+  isFirefoxPBM: undefined, // true, false, null
+  supportOn: false, // for support sidebar
+  supportFrom: "",
+  supportMessages: []
 });
 
 let dummyCount = 0;
 const dummyMessages = [
-	"店舗: すいません、店舗の登録でちょっと分からないところがあります。",
-	"",
-	"公開にはチェックを入れた方がいいですか？",
-	"サポーター: メニューは支払い方法の登録は終わっていますか？",
-	"店舗: いいえ。",
-	"サポーター: 通常、メニューなど用意した後に公開するので今は下書きとして保存して下さい。",
-	"店舗: 保存しました。次には何をしたら良いですか？",
-	"サポーター: 支払い方法の設定が必要になります。オンラインによる事前のクレジットカード払いは考えていますか？",
-	"店舗: 支払い方法には何がありますか？",
-	"サポーター: 現地での支払いかオンラインでクレジットカードによる事前支払いですね",
-	"店舗: 現地での支払いのみで考えています。",
-	"サポーター: では、Stripeの箇所は飛ばして受け取り払いを許可を選んで下さい。",
-	"店舗: 受け取りを許可にチェックを入れました。",
-	"店舗: 次はメニューを登録すれば良いでしょうか？",
-	"サポーター: はい、お持ち帰り商品を登録しましょう。",
-	"店舗: 登録完了しました",
-	"",
-	"店舗: これで終わりですか？",
-	"サポーター: はい、最後に掲載を申請して下さい",
-	"店舗: 申請しました",
-	"サポーター: 掲載までには少々時間がかかるのでお待ち下さい。"
+  {
+    role: "shop",
+    message:
+      "すみません、店舗の登録でちょっと分からないところがあります。公開にはチェックを入れた方がいいですか？"
+  },
+  {
+    role: "supporter",
+    message: "メニューは支払い方法の登録は終わっていますか？"
+  },
+  {
+    role: "shop",
+    message: "いいえ。"
+  },
+  {
+    role: "supporter",
+    message:
+      "通常、メニューなど用意した後に公開するので今は下書きとして保存して下さい。"
+  },
+  {
+    role: "shop",
+    message: "保存しました。次には何をしたら良いですか？"
+  },
+  {
+    role: "supporter",
+    message:
+      "支払い方法の設定が必要になります。オンラインによる事前のクレジットカード払いは考えていますか？"
+  },
+  {
+    role: "shop",
+    message: "支払い方法には何がありますか？"
+  },
+  {
+    role: "supporter",
+    message:
+      "現地での支払いかオンラインでクレジットカードによる事前支払いですね"
+  },
+  {
+    role: "shop",
+    message: "現地での支払いのみで考えています。"
+  },
+  {
+    role: "supporter",
+    message: "では、Stripeの箇所は飛ばして受け取り払いを許可を選んで下さい。"
+  },
+  {
+    role: "shop",
+    message:
+      "受け取りを許可にチェックを入れました。次はメニューを登録すれば良いでしょうか？"
+  },
+  {
+    role: "supporter",
+    message: "はい、お持ち帰り商品を登録しましょう。"
+  },
+  {
+    role: "shop",
+    message: "登録完了しました。これで終わりですか？"
+  },
+  {
+    role: "supporter",
+    message: "最後に掲載を申請して下さい。"
+  },
+  {
+    role: "shop",
+    message: "申請しました。"
+  },
+  {
+    role: "supporter",
+    message: "お疲れさまでした。掲載までには少々時間がかかるのでお待ち下さい。"
+  }
 ];
 
 export const getters = {
-	uid: (state) => {
-		return state.user && state.user.uid;
-	},
-	uidAdmin: (state) => {
-		return state.user && state.user.email && state.user.uid;
-	},
-	uidUser: (state) => {
-		return state.user && state.user.phoneNumber && state.user.uid;
-	},
-	userWasInitialized: (state) => {
-		// Check if state.user has been initialized (as the result of notication from Firebase)
-		return state.user !== undefined;
-	},
-	stripeRegion: (state) => {
-		return stripe_regions[state.server.region || "US"];
-	},
-	isSuperAdmin: (state) => {
-		return state.claims?.admin;
-	},
-	isNotSuperAdmin: (state) => {
-		return !state.claims?.admin;
-	},
-	isOperator: (state) => {
-		return state.claims?.operator;
-	},
-	isNotOperator: (state) => {
-		return !state.claims?.operator;
-	},
-	supportOn: (state) => {
-		return state.supportOn;
-	},
-	supportFrom: (state) => {
-		return state.supportFrom;
-	},
-	supportMessages: (state) => {
-		return state.supportMessages;
-	}
+  uid: state => {
+    return state.user && state.user.uid;
+  },
+  uidAdmin: state => {
+    return state.user && state.user.email && state.user.uid;
+  },
+  uidUser: state => {
+    return state.user && state.user.phoneNumber && state.user.uid;
+  },
+  userWasInitialized: state => {
+    // Check if state.user has been initialized (as the result of notication from Firebase)
+    return state.user !== undefined;
+  },
+  stripeRegion: state => {
+    return stripe_regions[state.server.region || "US"];
+  },
+  isSuperAdmin: state => {
+    return state.claims?.admin;
+  },
+  isNotSuperAdmin: state => {
+    return !state.claims?.admin;
+  },
+  isOperator: state => {
+    return state.claims?.operator;
+  },
+  isNotOperator: state => {
+    return !state.claims?.operator;
+  },
+  supportOn: state => {
+    return state.supportOn;
+  },
+  supportFrom: state => {
+    return state.supportFrom;
+  },
+  supportMessages: state => {
+    return state.supportMessages;
+  }
 };
 
 export const mutations = {
-	setActive(state, flag) {
-		state.isWindowActive = flag;
-	},
-	setLoading(state, flag) {
-		state.isLoading = flag;
-	},
-	setUser(state, user) {
-		state.user = user;
-	},
-	updateDate(state) {
-		state.date = new Date();
-	},
-	saveCart(state, payload) {
-		console.log("saving cart", payload.id, payload.cart);
-		state.carts = {};
-		state.carts[payload.id] = payload.cart;
-	},
-	setServerConfig(state, config) {
-		state.server = config;
-		console.log("store:setServerConfig", state.server.region);
-	},
-	setLang(state, lang) {
-		state.lang = lang;
-	},
-	setCustomClaims(state, claims) {
-		// Note: we can't copy user using Object.assign here
-		state.claims = claims;
-	},
-	pingOrderEvent(state) {
-		state.orderEvent = (state.orderEvent) + 1;
-	},
-	setOrders(state, orders) {
-		state.orderObj = orders.reduce((tmp, order) => {
-			const day = moment(order.timePlaced.toDate()).format("YYYY-MM-DD");
-			if (!tmp[day]) {
-				tmp[day] = [];
-			}
-			tmp[day].push(order);
-			return tmp;
-		}, {});
-	},
-	soundEnable(state) {
-		state.soundEnable = true;
-	},
-	setSoundOn(state, flag) {
-		state.soundOn = flag;
-	},
-	setSoundFile(state, file) {
-		state.soundFile = file;
-	},
-	resetDialog(state) {
-		state.dialog = null;
-	},
-	setAlert(state, params) {
-		state.dialog = { alert: params };
-	},
-	setErrorMessage(state, params) {
-		state.dialog = { error: params };
-	},
-	setFirefoxPBM(state, flag) {
-		if (flag === true) {
-			console.warn("Firefox Private Browsing Mode detected");
-		}
-		state.isFirefoxPBM = flag;
-	},
-	setSupportOn(state, flag) {
-		state.supportOn = flag;
-	},
-	setSupportFrom(state, pageName) {
-		// todo: 多言語対応
-		state.supportFrom = pageName;
-	},
-	setSupportMessage(state, message) {
-		state.supportMessages.push(message);
-	},
-	setDummySupportMessage(state) {
-		state.supportMessages.push(dummyMessages[dummyCount++]);
-	}
+  setActive(state, flag) {
+    state.isWindowActive = flag;
+  },
+  setLoading(state, flag) {
+    state.isLoading = flag;
+  },
+  setUser(state, user) {
+    state.user = user;
+  },
+  updateDate(state) {
+    state.date = new Date();
+  },
+  saveCart(state, payload) {
+    console.log("saving cart", payload.id, payload.cart);
+    state.carts = {};
+    state.carts[payload.id] = payload.cart;
+  },
+  setServerConfig(state, config) {
+    state.server = config;
+    console.log("store:setServerConfig", state.server.region);
+  },
+  setLang(state, lang) {
+    state.lang = lang;
+  },
+  setCustomClaims(state, claims) {
+    // Note: we can't copy user using Object.assign here
+    state.claims = claims;
+  },
+  pingOrderEvent(state) {
+    state.orderEvent = state.orderEvent + 1;
+  },
+  setOrders(state, orders) {
+    state.orderObj = orders.reduce((tmp, order) => {
+      const day = moment(order.timePlaced.toDate()).format("YYYY-MM-DD");
+      if (!tmp[day]) {
+        tmp[day] = [];
+      }
+      tmp[day].push(order);
+      return tmp;
+    }, {});
+  },
+  soundEnable(state) {
+    state.soundEnable = true;
+  },
+  setSoundOn(state, flag) {
+    state.soundOn = flag;
+  },
+  setSoundFile(state, file) {
+    state.soundFile = file;
+  },
+  resetDialog(state) {
+    state.dialog = null;
+  },
+  setAlert(state, params) {
+    state.dialog = { alert: params };
+  },
+  setErrorMessage(state, params) {
+    state.dialog = { error: params };
+  },
+  setFirefoxPBM(state, flag) {
+    if (flag === true) {
+      console.warn("Firefox Private Browsing Mode detected");
+    }
+    state.isFirefoxPBM = flag;
+  },
+  setSupportOn(state, flag) {
+    state.supportOn = flag;
+  },
+  setSupportFrom(state, pageName) {
+    // todo: 多言語対応
+    state.supportFrom = pageName;
+  },
+  setSupportMessage(state, message) {
+    state.supportMessages.push(message);
+  },
+  setDummySupportMessage(state) {
+    state.supportMessages.push(dummyMessages[dummyCount++]);
+  }
 };
 
-export const actions = {
-};
+export const actions = {};
